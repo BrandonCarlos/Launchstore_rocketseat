091dbc2e8c5bbd593dc5b3c6c89f18101994fb9b
Brandon Carlos <brandon_marzo@hotmail.com>
2021-12-05 13:05:34 -0300

    Launchstore Criando rota de create products

---
+++ src/lib/utils.js
@@ -0,0 +1,28 @@
+module.exports = {
+    date(timestamp) {
+        const date = new Date(timestamp)
+        const year = date.getUTCFullYear()
+        const month = `0${date.getUTCMonth() + 1}`.slice(-2)
+        const day = `0${date.getUTCDate()}`.slice(-2)
+
+        return {
+            iso: `${year}-${month}-${day}`,
+            birthDay: `${day}/${month}`,
+            format: `${day}/${month}/${year}`
+        }
+    },
+    blood(blood) {
+        const bloodType = {
+            A1: 'A+',
+            A0: 'A-',
+            B1: 'B+',
+            B0: 'B-',
+            AB1: 'AB+',
+            AB0: 'AB-',
+            O1: 'O+',
+            O0: 'O-'
+        }
+
+        return bloodType[blood]
+    }
+}
